f48fa03bdfec879c1ab0d2df42d322207db1c90c
mac <mac@macdeMacBook-Air.local>
2022-05-18 12:14:43 +0800

    hashRouter

---
+++ hashRouter/index.js
@@ -0,0 +1,79 @@
+const app = document.getElementById('app')
+const div1 = document.createElement('div')
+div1.innerHTML = '1.components'
+const div2 = document.createElement('div')
+div2.innerHTML = '2.components'
+const div3 = document.createElement('div')
+div3.innerHTML = '3.components'
+const div4 = document.createElement('div')
+div4.innerHTML = '4.components'
+
+const routerMap = {
+  "1": div1,
+  "2": div2,
+  "3": div3,
+  "4": div4
+}
+
+function route(container) {
+  // 获取路由hash 值
+  let hashName = window.location.hash.substr(1)
+  console.log(hashName)
+  hashName = hashName || '1'
+  // 获取路由对应的组件
+  const components = routerMap[hashName]
+  if(!components) {
+    // 404 todo
+    components = document.querySelector("#div404");
+  }
+  components.style.display = 'block'
+  console.log(components)
+
+  // 展示界面
+  container.innerHTML = "";
+  container.appendChild(components);
+}
+route(app)
+
+window.addEventListener('hashchange', () => {
+  route(app);
+})
+// const app = document.querySelector("#app");
+// const div1 = document.createElement("div");
+// div1.innerHTML = "1";
+// const div2 = document.createElement("div");
+// div2.innerHTML = "2";
+// const div3 = document.createElement("div");
+// div3.innerHTML = "3";
+// const div4 = document.createElement("div");
+// div4.innerHTML = "4";
+// const routeTable = {
+//   "1": div1,
+//   "2": div2,
+//   "3": div3,
+//   "4": div4
+// };
+
+// function route(container) {
+//   let number = window.location.hash.substr(1);
+
+//   number = number || 1;
+
+//   // 获取界面
+//   let div = routeTable[number.toString()];
+//   if (!div) {
+//     div = document.querySelector("#div404");
+//   }
+//   div.style.display = "block";
+
+//   // 展示界面
+//   container.innerHTML = "";
+//   container.appendChild(div);
+// }
+
+// route(app);
+
+// window.addEventListener("hashchange", () => {
+//   console.log("hash 变了");
+//   route(app);
+// });
