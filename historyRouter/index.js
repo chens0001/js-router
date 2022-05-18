fa52ec1658c9dfece1691186d6b48e9312f5ddad
mac <mac@macdeMacBook-Air.local>
2022-05-18 12:27:22 +0800

    history router

--- historyRouter/index.js
+++ historyRouter/index.js
@@ -9,32 +9,45 @@ const div4 = document.createElement('div')
 div4.innerHTML = '4.components'
 
 const routerMap = {
-  "1": div1,
-  "2": div2,
-  "3": div3,
-  "4": div4
-}
+  "/1": div1,
+  "/2": div2,
+  "/3": div3,
+  "/4": div4
+};
 
 function route(container) {
   // 获取路由hash 值
-  let hashName = window.location.hash.substr(1)
-  console.log(hashName)
-  hashName = hashName || '1'
+  let hashName = window.location.pathname;
+  if (hashName === "/") {
+    hashName = "/1";
+  }
+
   // 获取路由对应的组件
-  const components = routerMap[hashName]
+  let components = routerMap[hashName]
   if(!components) {
     // 404
     components = document.querySelector("#div404");
   }
   components.style.display = 'block'
-  console.log(components)
 
   // 展示界面
   container.innerHTML = "";
   container.appendChild(components);
 }
+
+const allA = document.querySelectorAll("a.link");
+for(let a of allA) {
+  a.addEventListener('click', ()=> {
+    e.preventDefault();
+    const href = a.getAttribute("href");
+    window.history.pushState(null, `page ${href}`, href);
+    // 路由变化更新
+    onStateChange(href);
+  })
+}
+
 route(app)
 
-window.addEventListener('hashchange', () => {
+function onStateChange() {
   route(app);
-})
+}
